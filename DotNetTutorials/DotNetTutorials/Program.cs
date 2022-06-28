09593ff9ddb82b89c5c51b577a94335e65744b24
Raghu Vamsi Gudala <rgudala@gategroup.com>
2022-06-28 22:54:33 +0530

    MultiThreading

---
+++ DotNetTutorials/DotNetTutorials/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace DotNetTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadStart obj = new ThreadStart(DisplayNumbers);
+            //Thread t1 = new Thread(obj);
+            Thread t1 = new Thread(new ParameterizedThreadStart(DisplayNumbers));
+            t1.Start(5);
+            Console.ReadKey(true);
+        }
+
+        static void DisplayNumbers(object max)
+        {
+            for (int i = 1; i <= Convert.ToInt32(max); i++)
+            {
+                Console.WriteLine("Method1 :" + i);
+            }
+        }
+    }
+}
